46d77f8eb65554b5fe72dcfd4c3526a9dd872868
xxrenzhe <yj2008ay611@gmail.com>
2014-08-19 16:59:54 +0800

    for branch first-five-minute

--- hadoop/files/nodemanager_healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Function : check nodemanager health
-# CTime : 2014.08.08
-# Author : Jason.Yu
-
-if [ `free -mlt |grep 'buffers/cache'|awk '{print $NF}'` -lt 512 ]
-then
-	echo "ERROR,free memory is lower than 512MB !"
-fi
